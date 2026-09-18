72862eb153ca0bb5431577fc3dad578b44ca5197
Yukitooo <yukibrent9@gmail.com>
2026-09-18 15:04:38 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const menuButton = document.querySelector('.menu-button');
+const mobileNav = document.querySelector('.mobile-nav');
+menuButton?.addEventListener('click', () => {
+  const open = mobileNav.classList.toggle('open');
+  menuButton.setAttribute('aria-expanded', String(open));
+});
+mobileNav?.querySelectorAll('a').forEach(link => link.addEventListener('click', () => {
+  mobileNav.classList.remove('open');
+  menuButton?.setAttribute('aria-expanded', 'false');
+}));
